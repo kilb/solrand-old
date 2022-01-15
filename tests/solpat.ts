486f4f1bb699567bc660247bce49cfbe86d87a8d
kilb <git@bll.io>
2022-01-15 22:29:26 +0800

    update

--- tests/solpat.ts
+++ tests/solpat.ts
@@ -2,62 +2,78 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Solpat } from '../target/types/solpat';
 import { TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
-import { NodeWallet } from '@project-serum/anchor/dist/cjs/provider';
+// import { NodeWallet } from '@project-serum/anchor/dist/cjs/provider';
 import { PublicKey, SystemProgram, Transaction, Connection, Commitment } from '@solana/web3.js';
 
-function toBeBytesU64( x ){
-  var bytes = [];
-  var i = 8;
-  do {
-    bytes[--i] = x & (255);
-    x = x>>8;
-  } while ( i )
-  return bytes;
-}
+const assert = require("assert");
 
 describe('solpat', () => {
-  const commitment: Commitment = 'processed';
-  const connection = new Connection('https://rpc-mainnet-fork.dappio.xyz', { commitment, wsEndpoint: 'wss://rpc-mainnet-fork.dappio.xyz/ws' });
-  const options = anchor.Provider.defaultOptions();
-  const wallet = NodeWallet.local();
-  const provider = new anchor.Provider(connection, wallet, options);
+  // const commitment: Commitment = 'processed';
+  // const connection = new Connection('https://rpc-mainnet-fork.dappio.xyz', { commitment, wsEndpoint: 'wss://rpc-mainnet-fork.dappio.xyz/ws' });
+  // const options = anchor.Provider.defaultOptions();
+  // const provider = new anchor.Provider(connection, wallet, options);
+
+  const priceFeedAccount = "FmAmfoyPXiA8Vhhe6MZTr3U6rZfEZ1ctEHay1ysqCqcf";
+  const AggregatorPublicKey = new PublicKey(priceFeedAccount);
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  // anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Solpat as Program<Solpat>;
+  const wallet = program.provider.wallet;
 
-  let token_mint = null as Token;
+  let myMint = null as Token;
   const admin = anchor.web3.Keypair.generate();
   const user = anchor.web3.Keypair.generate();
   
-  await provider.connection.confirmTransaction(
-    await provider.connection.requestAirdrop(admin.publicKey, 10000000000),
-    "processed"
-  );
-
-  await provider.connection.confirmTransaction(
-    await provider.connection.requestAirdrop(user.publicKey, 10000000000),
-    "processed"
-  );
-
-  token_mint = await Token.createMint(
-    provider.connection,
-    admin,
-    admin.publicKey,
-    null,
-    0,
-    TOKEN_PROGRAM_ID
-  );
   it('Create Pool', async () => {
-    // const seed = Buffer.from(toBeBytesU64(1));
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(admin.publicKey, 10000000000),
+      "processed"
+    );
+  
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(user.publicKey, 10000000000),
+      "processed"
+    );
+  
+    myMint = await Token.createMint(
+      provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
     let pool_id = new anchor.BN(1);
     const [pool_account_pda, _vault_account_bump] = await PublicKey.findProgramAddress(
       [pool_id.toBuffer("be", 8)],
       program.programId
     );
     // Add your test here.
-    const tx = await program.rpc.createPool({});
+    const tx = await program.rpc.createPool(
+      pool_id,
+      new anchor.BN(300), // duration: 300s
+      new anchor.BN(10), // fee_rate: 10/10000
+      {
+        accounts: {
+          authority: wallet.publicKey,
+          pool: pool_account_pda,
+          feedAccount: AggregatorPublicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          tokenMint: myMint.publicKey,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+        }
+      });
     console.log("Your transaction signature", tx);
+    let poolAccount = await program.account.pool.fetch(pool_account_pda);
+    assert.ok(
+      poolAccount.authority.equals(wallet.publicKey)
+    );
   });
 });
